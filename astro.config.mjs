bc828c4af751890ec6470b3dae8336fa5c6cec89
Muhammad Fahri Ramadhan <fahri@code.id>
2025-07-19 11:58:32 +0700

    feat: Upgrade astro to 5.12.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,16 @@ import { defineConfig } from 'astro/config';
 
 import svelte from '@astrojs/svelte';
 import tailwindcss from '@tailwindcss/vite';
-import vercelStatic from '@astrojs/vercel/static';
-
 import sitemap from '@astrojs/sitemap';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://link.fahri-r.com',
-	integrations: [svelte(), sitemap()],
-	vite: {
-		plugins: [tailwindcss()]
-	},
-	output: 'static',
-	adapter: vercelStatic()
+    site: 'https://link.fahri-r.com',
+    integrations: [svelte(), sitemap()],
+    vite: {
+        plugins: [tailwindcss()]
+    },
+    output: 'static',
+    adapter: cloudflare()
 });
